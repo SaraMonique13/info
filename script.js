cb1b8b71d62a591852e768d51ec12bd764a5f270
SaraMonique13 <moniquesarah5@gmail.com>
2024-04-11 12:37:16 -0300

    add

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function toggleMode() {
   //substituir a imagem
   if (html.classList.contains("light")) {
     //se tiver light mode add img light
-    img.setAttribute("src", "./ligh novo.png")
+    img.setAttribute("src", "./ligh mode new.png")
   } else {
     //se tiver sem light mode manter a imagem normal
     img.setAttribute("src", "./dark mode new.png")
